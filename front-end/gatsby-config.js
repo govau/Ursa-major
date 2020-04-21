d0c7f8ad60a099a3126278a3f3249762e000a978
Brendan Ng <brendan.ng@digital.gov.au>
2020-04-21 16:05:39 +1000

    Update gatsby-config.js

--- front-end/gatsby-config.js
+++ front-end/gatsby-config.js
@@ -47,14 +47,11 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "G-0RP5C8YQRX",
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },

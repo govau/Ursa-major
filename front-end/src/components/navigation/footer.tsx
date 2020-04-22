9930b7ea5fa365589770d158fdc21dcca97389f1
sukhrajghuman <sukhrajghuman@live.com>
2020-04-23 09:11:46 +1000

    Tidy up markdown directory, automate footer links, update instructions

--- front-end/src/components/navigation/footer.tsx
+++ front-end/src/components/navigation/footer.tsx
@@ -2,13 +2,35 @@ import React from "react";
 
 import AUfooter, { AUfooterNav, AUfooterEnd } from "../../auds/react/footer";
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+
 interface Props {}
+interface FooterLinks {
+  map(arg0: (item: any) => JSX.Element): React.ReactNode;
+  items: Array<Object>;
+}
 
 const AuFooter: any = AUfooter;
 const FooterNav: any = AUfooterNav;
 const FooterEnd: any = AUfooterEnd;
 
 const Footer: React.FC<Props> = () => {
+  const data = useStaticQuery(graphql`
+    query footerLinks {
+      site {
+        siteMetadata {
+          title
+          footerLinks {
+            text
+            link
+          }
+        }
+      }
+    }
+  `);
+
+  const Links: FooterLinks = data.site.siteMetadata.footerLinks;
+
   return (
     <>
       <div className="au-body au-body--dark">
@@ -19,12 +41,11 @@ const Footer: React.FC<Props> = () => {
                 <div className="col-md-3 col-sm-6">
                   <h3 className="au-display-lg">Section</h3>
                   <ul className="au-link-list au-link-list--inline">
-                    <li>
-                      <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                      <Link to="/contact">Contact</Link>
-                    </li>
+                    {Links.map((item: any) => (
+                      <li>
+                        <Link to={item.link}>{item.text}</Link>
+                      </li>
+                    ))}
                   </ul>
                 </div>
               </div>

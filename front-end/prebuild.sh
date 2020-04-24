818e7eeba3aa49bf9cec10e22335767920454a17
sukhrajghuman <sukhrajghuman@live.com>
2020-04-24 16:23:22 +1000

    hotfix add route and switch if else statements

--- front-end/prebuild.sh
+++ front-end/prebuild.sh
@@ -2,7 +2,7 @@
 
 if [ "$CIRCLE_BRANCH" = "master" ]
 then
-    export GATSBY_API_URL="https://ursa-major-front-end.apps.y.cld.gov.au/api"
-else
     export GATSBY_API_URL="https://analytics.service.gov.au/api"
+else
+    export GATSBY_API_URL="https://ursa-major-front-end.apps.y.cld.gov.au/api"
 fi

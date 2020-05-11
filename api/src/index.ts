0a3354144048bafc8b68cf1682b3e1029f67b2cc
sukhrajghuman <sukhrajghuman@live.com>
2020-05-11 14:40:48 +1000

    add redis service instance dev

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,30 +1,30 @@
 import express, { Application } from "express";
 import graphqlHTTP from "express-graphql";
-// const expressGraphQL = require("express-graphql")
 import Rschema from "./schema";
 import cors from "cors";
 import helmet from "helmet";
 import redis from "redis";
-
+import cfenv from "cfenv";
 import bodyParser from "body-parser";
+var env = process.env.NODE_ENV || "dev";
+
+const appEnv: any = cfenv.getAppEnv();
+const { hostname, port, password } = appEnv.services["redis"][0].credentials;
 
 const PORT: Number | string = process.env.PORT || 3000;
 
-const client = redis.createClient({ port: 6379 });
+const client =
+  env === "dev"
+    ? redis.createClient({ port: 6379 })
+    : redis.createClient({ host: hostname, port, password });
+
 const app: Application = express();
+
+env === "dev" && app.use(cors());
+
 app.use(bodyParser.json());
 
-var env = process.env.NODE_ENV || "dev";
-env === "dev" && app.use(cors());
 app.use(helmet());
-
-const getRedis = (req: any, res: any, next: any) => {
-  client.get("name", (err, res) => {
-    console.log(res);
-  });
-
-  next();
-};
 
 app.use(
   "/api",
